fcea4dd83410c43754937253329fecc68422b754
Krista <krista.loven.87@gmail.com>
2019-07-02 00:01:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,5 +23,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash.key?(key) ? hash[key] += 1 : hash[key] = 1
+  if hash.key?(key)
+    hash[key] += 1 
+  else
+    hash[key] = 1
+  end
 end
